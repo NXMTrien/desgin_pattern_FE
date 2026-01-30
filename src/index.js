9ed3bbde4bb191022d4466e685c486309a57b1ce
NXMTrien <minhtrien1322@gmail.com>
2026-01-30 21:47:26 +0700

    cap_nhat_30/1/26 moi

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// console.log(
+//   "Google Client ID:",
+//   process.env.REACT_APP_GOOGLE_CLIENT_ID
+// );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
     <BrowserRouter>
-      <App />
+      <App  />
     </BrowserRouter>
   </GoogleOAuthProvider>
   </React.StrictMode>
